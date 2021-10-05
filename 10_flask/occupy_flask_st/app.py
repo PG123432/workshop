beaade4bd24f2a852d63a47ac2bfe21adb10bdc6
Patrick Ging <user@PatsMacBook-Pro.fios-router.home>
2021-10-05 05:26:14 -0400

    Adding more rendering

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -13,6 +13,12 @@ def index():
             csv_reader = csv.reader(f, delimiter=",") 
             next(csv_reader)
             sum = 0.0
+
+
+            returnString = '<h2>The Nameless Team: Patrick Ging, Eric Guo, Hebe Huang</h2> </hr>' #bottom part with our job
+
+            jobTable = "<table> <tr> <th> Possible Jobs </th> </tr>" #top part with table
+
             
             for value in csv_reader:
                             if(value[0] == "Total"):
@@ -20,13 +26,19 @@ def index():
                             sum = sum + (10 * float(value[1])) #adding the values
                             job_and_prob[sum] = value[0]
 
-
+                            jobTable += "\n<tr> <td> " + value[0] + " </tr></td>"
         
 
             randNum = random.randrange(998) #random number
             for percent in job_and_prob.keys():
-                if(percent >= randNum): #if it's below the value that means it's in between it and the value before it - elsewise we know it's above the value so we don't actually need another logic operator
-                        return(job_and_prob.get(percent))
+                if(percent >= randNum):
+                    returnString += "<h2> Your job is {} </h2></hr>".format(job_and_prob[percent])
+                    break
+                    #if it's below the value that means it's in between it and the value before it -
+                    #elsewise we know it's above the value so we don't actually need another logic operator
+
+            return returnString + jobTable
+                
                         
 if __name__ == "__main__":  
     app.debug = True       
